63ffdc3c0b2eff821683251107aeb591e474b405
yucelenn <yucelenn@hotmail.com>
2024-03-16 00:51:21 +0300

    Employer add method added
    PostMapping add method implemented from JPA

--- src/main/java/kodlamaio/hrms/api/controllers/EmployersController.java
+++ src/main/java/kodlamaio/hrms/api/controllers/EmployersController.java
@@ -3,11 +3,14 @@ package kodlamaio.hrms.api.controllers;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.hrms.business.abstracts.EmployerService;
 import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.Employer;
 
 @RestController
@@ -24,6 +27,12 @@ public class EmployersController {
 	@GetMapping("/getall")
 	DataResult<List<Employer>> getAll(){
 		return this.employerService.getAll();
+	}
+	
+	@PostMapping("/add")
+	public Result add(@RequestBody Employer employer) {
+		return this.employerService.add(employer);
+		
 	}
 
 }
--- src/main/java/kodlamaio/hrms/business/concretes/EmployerManager.java
+++ src/main/java/kodlamaio/hrms/business/concretes/EmployerManager.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Service;
 
 import kodlamaio.hrms.business.abstracts.EmployerService;
 import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
+import kodlamaio.hrms.core.utilities.results.SuccessResult;
 import kodlamaio.hrms.dataAccess.abstracts.EmployerDao;
 import kodlamaio.hrms.entities.concretes.Employer;
 
@@ -23,6 +25,12 @@ public class EmployerManager implements EmployerService{
 	@Override
 	public DataResult<List<Employer>> getAll() {
 		return new SuccessDataResult<List<Employer>>(employerDao.findAll(), "Data listelendi.");
+	}
+
+	@Override
+	public Result add(Employer employer) {
+		this.employerDao.save(employer);
+		return new SuccessResult("İş veren eklendi.");
 	}
 
 }
