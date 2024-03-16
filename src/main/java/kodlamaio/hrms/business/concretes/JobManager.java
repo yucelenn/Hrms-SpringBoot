daf9dbd5dec159e0e0bac4e260fb6c113cbb4478
yucelenn <yucelenn@hotmail.com>
2024-03-16 15:41:45 +0300

    Job add,-getAll methods, JobsController added
    JobService, JobManager, JobsController and Job add-getAll methos added

---
+++ src/main/java/kodlamaio/hrms/business/concretes/JobManager.java
@@ -0,0 +1,38 @@
+package kodlamaio.hrms.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlamaio.hrms.business.abstracts.JobService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
+import kodlamaio.hrms.core.utilities.results.SuccessResult;
+import kodlamaio.hrms.dataAccess.abstracts.JobDao;
+import kodlamaio.hrms.entities.concretes.Job;
+
+@Service
+public class JobManager implements JobService {
+
+	private JobDao jobDao;
+	
+	@Autowired
+	public JobManager(JobDao jobDao) {
+		super();
+		this.jobDao = jobDao;
+	}
+
+	@Override
+	public DataResult<List<Job>> getAll() {
+		return new SuccessDataResult<List<Job>>(this.jobDao.findAll(), "Data listelendi.");
+	}
+
+	@Override
+	public Result add(Job job) {
+		this.jobDao.save(job);
+		return new SuccessResult("İş ünvanı eklendi.");
+	}
+
+}
