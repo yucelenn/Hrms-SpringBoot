465f896ad96dab28f19e9958f6abb86d9c7c0217
yucelenn <yucelenn@hotmail.com>
2024-04-04 21:14:32 +0300

    checkMailMatchesDomain method added

--- src/main/java/kodlamaio/hrms/business/abstracts/checkServices/EmployerCheckService.java
+++ src/main/java/kodlamaio/hrms/business/abstracts/checkServices/EmployerCheckService.java
@@ -2,4 +2,6 @@ package kodlamaio.hrms.business.abstracts.checkServices;
 
 public interface EmployerCheckService extends CheckService {
 
+	boolean checkMailMatchesDomain(String eMail, String domain);
+
 }
--- src/main/java/kodlamaio/hrms/business/concretes/checkManagers/EmployerCheckManager.java
+++ src/main/java/kodlamaio/hrms/business/concretes/checkManagers/EmployerCheckManager.java
@@ -6,8 +6,15 @@ public class EmployerCheckManager implements EmployerCheckService{
 
 	@Override
 	public boolean checkMailIsUnique(String eMail) {
-		// TODO Auto-generated method stub
-		return false;
+		// bu metodu doldur jpa ile
+		return true;
+	}
+
+	@Override
+	public boolean checkMailMatchesDomain(String eMail, String domain) {
+		String[] emails = eMail.split("[@.]+");
+        String[] domains = domain.split("\\.");
+        return ( domains[0].equalsIgnoreCase(emails[1]) || domains[1].equalsIgnoreCase(emails[1]));
 	}
 
 }
