c27c64f3e063a2c88dca7ac2c1ff1d51c3df08e5
yucelenn <yucelenn@hotmail.com>
2024-04-24 23:08:13 +0300

    CandidateCheckManager checkIdentityNumberIsUnique method updated

--- src/main/java/kodlamaio/hrms/business/concretes/checkManagers/CandidateCheckManager.java
+++ src/main/java/kodlamaio/hrms/business/concretes/checkManagers/CandidateCheckManager.java
@@ -75,9 +75,9 @@ public class CandidateCheckManager implements CandidateCheckService{
 			if ( !(mernisVerification.checkIfRealPerson(candidate)) ) { //mernis doğrulaması false ise
 				return new ErrorResult("İş arayan eklenemedi, Bilgileriniz Mernis sistemi ile uyuşmuyor!"); 
 			}
-			//else if ( !(this.checkMailIsUnique(candidate.getEMail()))  ) { //mail daha önce kullanılmışsa
-			//	return new ErrorResult("İş arayan eklenemedi, Bu e posta daha önce kullanılmış!!");
-			//}
+			else if ( !(this.checkMailIsUnique(candidate.getEMail()))  ) { //mail daha önce kullanılmışsa
+				return new ErrorResult("İş arayan eklenemedi, Bu e posta daha önce kullanılmış!!");
+			}
 			else if ( !(checkIdentityNumberIsUnique(candidate.getIdentityNumber())) ) { //tc no daha önce kullanılmışsa
 				return new ErrorResult("İş arayan eklenemedi, Bu Tc kimlik numarası daha önce kullanılmış!");
 			}
