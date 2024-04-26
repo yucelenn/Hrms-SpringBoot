97f2d87432de0f3fb5e5541a9b2cea9ced00ff49
yucelenn <yucelenn@hotmail.com>
2024-04-26 14:37:19 +0300

    Dependency Injection-JobCheckManager checkJobNameIsUnique method updated

--- src/main/java/kodlamaio/hrms/business/concretes/checkManagers/JobCheckManager.java
+++ src/main/java/kodlamaio/hrms/business/concretes/checkManagers/JobCheckManager.java
@@ -1,26 +1,39 @@
 package kodlamaio.hrms.business.concretes.checkManagers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlamaio.hrms.business.abstracts.checkServices.JobCheckService;
 import kodlamaio.hrms.core.utilities.results.ErrorResult;
 import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.core.utilities.results.SuccessResult;
+import kodlamaio.hrms.dataAccess.abstracts.JobDao;
 import kodlamaio.hrms.entities.concretes.Job;
 
 @Service
 public class JobCheckManager implements JobCheckService{
 
+	private JobDao jobDao;
+	
+	@Autowired
+	public JobCheckManager(JobDao jobDao) {
+		super();
+		this.jobDao = jobDao;
+	}
+
 	@Override
 	public boolean checkJobNameIsUnique(String jobName) {
-		// burayı JPA ile doldur.
-		return true;
+		if (this.jobDao.getByJobTitle(jobName) != null) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 	
 	@Override
 	public Result checkInformationsFulfilled(Job job) {
 		if (job.getJobTitle().isEmpty()) {
-			return new ErrorResult("Lütfen iş pozisyonu ismini giriniz!");
+			return new ErrorResult("Lütfen iş pozisyonu ismi giriniz!");
 		} else {
 			return new SuccessResult("Bilgi girişi başarılı!");
 		}
