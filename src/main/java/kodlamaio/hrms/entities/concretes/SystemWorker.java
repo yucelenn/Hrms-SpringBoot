77dc59bb8cf08a8aefa82ddae5ea2fcca94b3a3f
yucelenn <yucelenn@hotmail.com>
2024-04-28 00:41:04 +0300

    SystemWorker updated with NotBlank annotations

--- src/main/java/kodlamaio/hrms/entities/concretes/SystemWorker.java
+++ src/main/java/kodlamaio/hrms/entities/concretes/SystemWorker.java
@@ -3,6 +3,7 @@ package kodlamaio.hrms.entities.concretes;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -22,9 +23,11 @@ public class SystemWorker extends User{
 	private int systemWorkerId;*/
 	
 	@Column(name="first_name")
+	@NotBlank(message = "İsim alanı boş bırakılamaz!")
 	private String firstName;
 	
 	@Column(name="last_name")
+	@NotBlank(message = "Soy isim alanı boş bırakılamaz!")
 	private String lastName;
 
 }
