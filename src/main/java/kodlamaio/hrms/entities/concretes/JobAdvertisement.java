cda7549ea99ae61eb492d724bab5c91c633bd0dc
yucelenn <yucelenn@hotmail.com>
2024-04-28 00:09:50 +0300

    JobAdvertisement class added

---
+++ src/main/java/kodlamaio/hrms/entities/concretes/JobAdvertisement.java
@@ -0,0 +1,51 @@
+package kodlamaio.hrms.entities.concretes;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "job_advertisements")
+public class JobAdvertisement {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "description")
+	private String description;
+
+	@Column(name = "max_salary")
+	private int maxSalary;
+	
+	@Column(name = "min_salary")
+	private int minSalary;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name = "start_date")
+	private Date startDate;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name = "end_date")
+	private Date endDate;
+	
+	@Column(name = "status")
+	private boolean status;
+	
+	@Column(name = "free_position_amount")
+	private int freePositionAmount;
+}
