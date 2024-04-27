2e4eeb36c3c62bc45abe2a4367d4ed5e3cb5e63b
yucelenn <yucelenn@hotmail.com>
2024-04-28 00:03:33 +0300

    City class added

---
+++ src/main/java/kodlamaio/hrms/entities/concretes/City.java
@@ -0,0 +1,27 @@
+package kodlamaio.hrms.entities.concretes;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "cities")
+public class City {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "name")
+	private String cityName;
+}
