d86e9a8e4af2bf495a653bc970d5dc8ebd8e2d7c
yucelenn <yucelenn@hotmail.com>
2024-04-29 23:08:56 +0300

    verifications package, EmailVerification class added

---
+++ src/main/java/kodlamaio/hrms/entities/concretes/verifications/EmailVerification.java
@@ -0,0 +1,38 @@
+package kodlamaio.hrms.entities.concretes.verifications;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "verification_codes")
+public class EmailVerification {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "code")
+	private String code;
+	
+	@Column(name = "is_verified")
+	private boolean isVerified;
+	
+	@Column(name = "verified_date")
+	private Date verifiedDate;
+}
