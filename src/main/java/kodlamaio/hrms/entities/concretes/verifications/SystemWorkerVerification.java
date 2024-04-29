9e963560208e3b31745b6fea36f7a18ed20ff882
yucelenn <yucelenn@hotmail.com>
2024-04-30 00:13:27 +0300

    SystemWorkerVerification class added

---
+++ src/main/java/kodlamaio/hrms/entities/concretes/verifications/SystemWorkerVerification.java
@@ -0,0 +1,38 @@
+package kodlamaio.hrms.entities.concretes.verifications;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Inheritance(strategy = InheritanceType.JOINED)
+@Entity
+@Table(name = "employee_confirms")
+public class SystemWorkerVerification {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "employee_id")
+	private int employeeId;
+	
+	@Column(name = "is_confirmed")
+	private boolean isConfirmed;
+	
+	@Column(name = "confirm_date")
+	private Date confirmDate;
+}
