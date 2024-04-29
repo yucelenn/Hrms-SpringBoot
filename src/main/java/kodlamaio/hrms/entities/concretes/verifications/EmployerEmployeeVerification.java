bd38e673e0baeccda20f1b54ba2cf85ec982e9a1
yucelenn <yucelenn@hotmail.com>
2024-04-30 00:29:18 +0300

    EmployerEmployeeVerification class added

---
+++ src/main/java/kodlamaio/hrms/entities/concretes/verifications/EmployerEmployeeVerification.java
@@ -0,0 +1,26 @@
+package kodlamaio.hrms.entities.concretes.verifications;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import kodlamaio.hrms.entities.concretes.Employer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Table(name = "employee_confirms_employers")
+public class EmployerEmployeeVerification extends EmployeeVerification {
+
+	@OneToOne
+	@JoinColumn(name="employer_id")
+	@ToString.Exclude	
+	private Employer employer;
+}
