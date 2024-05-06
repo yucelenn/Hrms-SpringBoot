025d9b9108f4723462e052b3d4128e9b83dac3d4
yucelenn <yucelenn@hotmail.com>
2024-05-06 16:19:29 +0300

    ResponseEntity add method, handleValidationException method added

--- src/main/java/kodlamaio/hrms/api/controllers/UsersController.java
+++ src/main/java/kodlamaio/hrms/api/controllers/UsersController.java
@@ -1,17 +1,27 @@
 package kodlamaio.hrms.api.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import kodlamaio.hrms.business.abstracts.UserService;
 import kodlamaio.hrms.core.utilities.results.DataResult;
-import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.core.utilities.results.ErrorDataResult;
 import kodlamaio.hrms.entities.concretes.User;
 
 @RestController
@@ -20,19 +30,31 @@ public class UsersController {
 
 	private UserService userService;
 	
+	@Autowired
 	public UsersController(UserService userService) {
 		super();
 		this.userService = userService;
 	}
 
+	@ExceptionHandler(MethodArgumentNotValidException.class) //sistemde hata oluşursa bu metodu çağırır
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ErrorDataResult<Object> handleValidationException(MethodArgumentNotValidException exceptions){ //oluşan hatayı method parametresi olarak geçecek
+		Map<String, String> validationErrors = new HashMap<String, String>();
+		for ( FieldError fieldError : exceptions.getBindingResult().getFieldErrors() ) {
+			validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());//entitylerdeki NotBlank leri çalıştıracak
+		}
+		ErrorDataResult<Object> errors = new ErrorDataResult<Object>(validationErrors, "Doğrulama hataları");
+		return errors;		
+	}
+	
 	@GetMapping("/getall")
 	public DataResult<List<User>> getAll(){
 		return this.userService.getAll();
 	}
 	
 	@PostMapping("/add")
-	public Result add(@RequestBody User user) {
-		return this.userService.add(user);	
+	public ResponseEntity<?> add (@Valid @RequestBody User user) {
+		return ResponseEntity.ok(this.userService.add(user));	
 	}
 	
 	@GetMapping("/getByeMail")
