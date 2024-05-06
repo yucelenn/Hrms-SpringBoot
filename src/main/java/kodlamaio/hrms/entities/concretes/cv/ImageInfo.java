bdd3ab6a642ebb1b99dbaf389029c947dad90f77
yucelenn <yucelenn@hotmail.com>
2024-05-06 18:05:59 +0300

    ImageInfo class added to cv package

---
+++ src/main/java/kodlamaio/hrms/entities/concretes/cv/ImageInfo.java
@@ -0,0 +1,45 @@
+package kodlamaio.hrms.entities.concretes.cv;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import kodlamaio.hrms.entities.concretes.Candidate;
+import kodlamaio.hrms.entities.concretes.cv.enums.StorageType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "image_infos")
+public class ImageInfo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "image_name")
+	private String imageName;
+	
+	@Column(name = "image_path")
+	private String imagePath;
+	
+	@Column(name = "storage_name")
+	@Enumerated(EnumType.STRING)
+	private StorageType storageName;
+	
+	@ManyToOne()
+	@JoinColumn(name = "candidate_id")
+	private Candidate candidate;
+	
+}
