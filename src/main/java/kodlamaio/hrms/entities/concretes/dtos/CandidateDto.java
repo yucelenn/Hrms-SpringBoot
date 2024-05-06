2d0c80b899e847db2c18df0bff8f4e0f3f4b02e1
yucelenn <yucelenn@hotmail.com>
2024-05-07 00:54:52 +0300

    CandidateDto added to entities.concretes.dtos package

---
+++ src/main/java/kodlamaio/hrms/entities/concretes/dtos/CandidateDto.java
@@ -0,0 +1,25 @@
+package kodlamaio.hrms.entities.concretes.dtos;
+
+import java.util.Date;
+
+public class CandidateDto {
+
+	private int id;
+	private String schoolName;
+	private String departmantName;
+	private String degree;
+	private Date startDate;
+	private Date endDate;
+	private String companyName;
+	private String role;
+	private Date jobStartDate;
+	private Date jobEndDate;
+	private String languageName;
+	private int languageLevel;
+	private String imagePath;
+	private String githubLink;
+	private String linkedinLink;
+	private String technology;
+	private String coverLetterDescription;
+
+}
