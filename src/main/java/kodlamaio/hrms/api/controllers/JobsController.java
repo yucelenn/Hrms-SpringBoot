4c3fe91cdcd6ed41cb41da9793d392e5d09bccbd
yucelenn <yucelenn@hotmail.com>
2024-05-06 16:02:25 +0300

    handleValidationException method added,add method updated(ResponseEntity

--- src/main/java/kodlamaio/hrms/api/controllers/JobsController.java
+++ src/main/java/kodlamaio/hrms/api/controllers/JobsController.java
@@ -1,17 +1,27 @@
 package kodlamaio.hrms.api.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import kodlamaio.hrms.business.abstracts.JobService;
 import kodlamaio.hrms.core.utilities.results.DataResult;
-import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.core.utilities.results.ErrorDataResult;
 import kodlamaio.hrms.entities.concretes.Job;
 
 @RestController
@@ -20,9 +30,21 @@ public class JobsController {
 	
 	private JobService jobService;
 
+	@Autowired
 	public JobsController(JobService jobService) {
 		super();
 		this.jobService = jobService;
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class) //sistemde hata oluşursa bu metodu çağırır
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ErrorDataResult<Object> handleValidationException(MethodArgumentNotValidException exceptions){ //oluşan hatayı method parametresi olarak geçecek
+		Map<String, String> validationErrors = new HashMap<String, String>();
+		for ( FieldError fieldError : exceptions.getBindingResult().getFieldErrors() ) {
+			validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());//entitylerdeki NotBlank leri çalıştıracak
+		}
+		ErrorDataResult<Object> errors = new ErrorDataResult<Object>(validationErrors, "Doğrulama hataları");
+		return errors;		
 	}
 	
 	@GetMapping("/getall")
@@ -31,8 +53,8 @@ public class JobsController {
 	}
 	
 	@PostMapping("/add")
-	public Result add(@RequestBody Job job) {
-		return this.jobService.add(job);	
+	public ResponseEntity<?> add(@Valid @RequestBody Job job) {
+		return ResponseEntity.ok(this.jobService.add(job));	
 	}
 
 	@GetMapping("/getByJobTitle")
