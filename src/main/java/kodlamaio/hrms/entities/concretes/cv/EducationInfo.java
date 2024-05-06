93596be8be56500d4e28831badec48e17c00cdd2
yucelenn <yucelenn@hotmail.com>
2024-05-06 16:56:43 +0300

    entities.concretes.cv package, EducationInfo class added

---
+++ src/main/java/kodlamaio/hrms/entities/concretes/cv/EducationInfo.java
@@ -0,0 +1,53 @@
+package kodlamaio.hrms.entities.concretes.cv;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import kodlamaio.hrms.entities.concretes.Candidate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "cv_education_info")
+public class EducationInfo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "school_name")
+	private String schoolName;
+	
+	@Column(name = "departmant_name")
+	private String departmantName;
+	
+	@Column(name = "degree")
+	private String degree;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name = "start_date")
+	private Date startDate;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name = "end_date")
+	private Date endDate;
+	
+	@ManyToOne()
+	@JoinColumn(name = "job_seeker_id")
+	private Candidate candidate;
+	
+}
