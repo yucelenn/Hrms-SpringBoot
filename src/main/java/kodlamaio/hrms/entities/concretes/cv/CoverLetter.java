3739606aa5efac3f47e2281ad1dd1b1b533fcda8
yucelenn <yucelenn@hotmail.com>
2024-05-06 17:28:36 +0300

    CoverLetter class added

---
+++ src/main/java/kodlamaio/hrms/entities/concretes/cv/CoverLetter.java
@@ -0,0 +1,35 @@
+package kodlamaio.hrms.entities.concretes.cv;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import kodlamaio.hrms.entities.concretes.Candidate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "cover_letters")
+public class CoverLetter {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "cover_letter_description")
+	private String description;
+	
+	@OneToOne()
+	@JoinColumn(name = "candidate_id")
+	private Candidate candidate;
+	
+}
