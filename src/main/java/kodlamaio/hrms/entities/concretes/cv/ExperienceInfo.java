f69dcc6403dd925e85ed23113dff5e4e9270b814
yucelenn <yucelenn@hotmail.com>
2024-05-06 17:16:34 +0300

    ExperienceInfo class added

---
+++ src/main/java/kodlamaio/hrms/entities/concretes/cv/ExperienceInfo.java
@@ -0,0 +1,50 @@
+package kodlamaio.hrms.entities.concretes.cv;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import kodlamaio.hrms.entities.concretes.Candidate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "cv_experience_info")
+public class ExperienceInfo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "company_name")
+	private String companyName;
+	
+	@Column(name = "role")
+	private String role;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name = "start_date")
+	private Date startDate;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name = "end_date")
+	private Date endDate;
+	
+	@ManyToOne
+	@JoinColumn(name = "candidate_id")
+	private Candidate candidate;
+	
+}
