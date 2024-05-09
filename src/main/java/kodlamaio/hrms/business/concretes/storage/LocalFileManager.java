454a3ba694737dea149c388e72112c425c79daf9
yucelenn <yucelenn@hotmail.com>
2024-05-09 21:41:32 +0300

    storage package, LocalFileManager class added to business.concretes

---
+++ src/main/java/kodlamaio/hrms/business/concretes/storage/LocalFileManager.java
@@ -0,0 +1,27 @@
+package kodlamaio.hrms.business.concretes.storage;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import kodlamaio.hrms.business.abstracts.storage.FileService;
+import kodlamaio.hrms.entities.concretes.cv.enums.StorageType;
+
+public class LocalFileManager implements FileService{
+
+	@Override
+	public String uploadFile(MultipartFile file) throws IOException {
+		File convFile = new File("src/main/resources/temporaryFolder/"+file.getOriginalFilename());
+		String path = convFile.getAbsolutePath();
+		file.transferTo(Path.of(convFile.getAbsolutePath()));
+		return path;
+	}
+
+	@Override
+	public StorageType getFileStorageName() {
+		return StorageType.Local;
+	}
+
+}
