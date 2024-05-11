f68d9fb5dd3d8f2e3a1e1c1e455dcaa5ee29c8ec
yucelenn <yucelenn@hotmail.com>
2024-05-12 00:27:48 +0300

    LanguagesController class added to api.controllers.cvControllers package

---
+++ src/main/java/kodlamaio/hrms/api/controllers/cvControllers/LanguagesController.java
@@ -0,0 +1,39 @@
+package kodlamaio.hrms.api.controllers.cvControllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.hrms.business.abstracts.cvServices.LanguageService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.entities.concretes.cv.Language;
+
+@RestController
+@RequestMapping(value = "/api/languages")
+public class LanguagesController {
+
+	private LanguageService languageService;
+
+	@Autowired
+	public LanguagesController(LanguageService languageService) {
+		super();
+		this.languageService = languageService;
+	}
+	
+	@PostMapping("/add")
+	public Result add(@RequestBody Language language) {
+		return this.languageService.add(language);	
+    }
+	
+	@GetMapping("/getAll")
+	public DataResult<List<Language>> getAll(){
+		return this.languageService.getAll();
+	}
+	
+}
