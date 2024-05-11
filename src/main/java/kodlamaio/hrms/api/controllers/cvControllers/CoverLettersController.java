bbfb46a3d2a83edee0bfea4cb5ed94ee458b124b
yucelenn <yucelenn@hotmail.com>
2024-05-11 20:41:31 +0300

    cvControllers package,CoverLettersController class added api.controllers

---
+++ src/main/java/kodlamaio/hrms/api/controllers/cvControllers/CoverLettersController.java
@@ -0,0 +1,10 @@
+package kodlamaio.hrms.api.controllers.cvControllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/coverletters")
+public class CoverLettersController {
+
+}
