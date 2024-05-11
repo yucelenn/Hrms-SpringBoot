e9acea3d9b2d80cea48c971eae21fa23ace84c99
yucelenn <yucelenn@hotmail.com>
2024-05-11 18:12:48 +0300

    List<CandidateDto> getCandidateCvDto() method and Query added

--- src/main/java/kodlamaio/hrms/dataAccess/abstracts/CandidateDao.java
+++ src/main/java/kodlamaio/hrms/dataAccess/abstracts/CandidateDao.java
@@ -3,8 +3,10 @@ package kodlamaio.hrms.dataAccess.abstracts;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import kodlamaio.hrms.entities.concretes.Candidate;
+import kodlamaio.hrms.entities.concretes.dtos.CandidateDto;
 
 public interface CandidateDao extends JpaRepository<Candidate, Integer>{
 
@@ -18,4 +20,8 @@ public interface CandidateDao extends JpaRepository<Candidate, Integer>{
 	List<Candidate> getByFirstNameStartsWith(String firstName);
 	List<Candidate> getByLastNameStartsWith(String lastName);
 	Candidate findByeMail(String eMail);
+	
+	@Query("SELECT new kodlamaio.hrms.entities.concretes.dtos.CandidateDto(js.id, j.schoolName, j.departmantName, j.degree, j.startDate, j.endDate, je.companyName, je.role, je.startDate, je.endDate, jl.languageName,jl.languageLevel, ji.imagePath, jw.githubLink, jw.linkedinLink, jt.technology, jc.description) FROM Candidate js JOIN js.educationInfos j JOIN js.experienceInfos je JOIN js.languages jl JOIN js.imageInfos ji JOIN js.webAddress jw JOIN js.talents jt JOIN js.coverLetter jc")
+	List<CandidateDto> getCandidateCvDto();
+	
 }
